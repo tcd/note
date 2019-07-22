538296a373cc58ba58289ae9ccc92b96423022c9
Clay Dunston <dunstontc@gmail.com>
2019-07-22 15:27:06 -0500

    Add `note.go` and `main.go`.
    This commit contains the initial, minimal version of `note`.

--- main.go
+++ main.go
@@ -1,8 +1,47 @@
 // note provides a command line interface for managing note files.
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var newFlag = flag.String("new", "", "Pass a string for the name of a new note")
+var listFlag = flag.Bool("list", false, "List all notes")
+
+func init() {
+	flag.StringVar(newFlag, "n", "", "")
+	flag.BoolVar(listFlag, "l", false, "")
+	flag.Usage = usage
+}
 
 func main() {
-	fmt.Println(getNoteDir())
+	flag.Parse()
+
+	if !fileExists(getNoteDir()) {
+		makeDir(getNoteDir())
+	}
+
+	if *listFlag {
+		for _, note := range ls(getNoteDir()) {
+			fmt.Println(note.Name())
+		}
+	} else if len(*newFlag) > 0 {
+		openFile(newNamedNote(*newFlag))
+	} else {
+		openFile(newDefaultNote())
+	}
+}
+
+func usage() {
+	title := `                __
+   ____  ____  / /____
+  / __ \/ __ \/ __/ _ \
+ / / / / /_/ / /_/  __/
+/_/ /_/\____/\__/\___/
+`
+	fmt.Println(title)
+	fmt.Printf("Usage: %s [OPTIONS] argument\n", os.Args[0])
+	flag.PrintDefaults()
 }
--- note.go
+++ note.go
@@ -1,19 +1,94 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
 	"os"
-	"path"
+	"os/exec"
+	"path/filepath"
+	"time"
 )
 
-// getNoteDir returns the full path of $NOTE_DIR, or ~/notes if no value is set.
-func getNoteDir() string {
-	noteDir := ""
-	envSetting := os.Getenv("NOTE_DIR")
-	if len(envSetting) > 0 {
-		noteDir += envSetting
-	} else {
-		home := os.Getenv("HOME")
-		noteDir = path.Join(home, "notes")
+func newNamedNote(name string) string {
+	note := filepath.Join(getNoteDir(), name)
+	fileName := addMdExtension(note)
+	createFile(fileName, ("# " + name + "\n"))
+	return fileName
+}
+
+func newDefaultNote() string {
+	note := defaultNoteName()
+	createFile(note, defaultNoteTitle())
+	return note
+}
+
+func openFile(filePath string) {
+	cmd := exec.Command(getEditor(), filePath)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
 	}
-	return noteDir
+}
+
+// createFile makes a file at name only if the file doesn't already exist.
+func createFile(name string, contents string) {
+	fileContent := []byte(contents)
+	permissions := os.FileMode(0666)
+
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		err := ioutil.WriteFile(name, fileContent, permissions)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+// ls returns an array of os.Fileinfo objects contained in a given directory.
+func ls(path string) []os.FileInfo {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return files
+}
+
+func defaultNoteTitle() string {
+	dateFmt := "January 2, 2006"
+	currentTime := time.Now()
+	return ("# " + currentTime.Format(dateFmt) + "\n\n")
+}
+
+func defaultNoteName() string {
+	currentTime := time.Now()
+	date := currentTime.Format("01-02-2006")
+	noteName := filepath.Join(getNoteDir(), date) + ".md"
+	return noteName
+}
+
+// Exists returns a true if a file exists and false if it doesn't.
+func fileExists(name string) bool {
+	_, err := os.Stat(name)
+	return os.IsNotExist(err)
+}
+
+// makeDir creates the directory at `name` if it doesn't already exist.
+func makeDir(name string) {
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		err := os.MkdirAll(name, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func addMdExtension(file string) string {
+	ext := filepath.Ext(file)
+	if ext != ".md" {
+		newName := file[0:len(file)-len(ext)] + ".md"
+		return newName
+	}
+	return file
 }
